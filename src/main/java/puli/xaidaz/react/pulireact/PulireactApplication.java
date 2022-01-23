ce5cc799d09dc1977a514c24a1b8dfe4fe2f30aa
Hanne Heingård <hahe@netcompany.com>
2022-01-23 19:05:35 +0100

    First commit

---
+++ src/main/java/puli/xaidaz/react/pulireact/PulireactApplication.java
@@ -0,0 +1,13 @@
+package puli.xaidaz.react.pulireact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulireactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PulireactApplication.class, args);
+    }
+
+}
---
+++ src/test/java/puli/xaidaz/react/pulireact/PulireactApplicationTests.java
@@ -0,0 +1,13 @@
+package puli.xaidaz.react.pulireact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulireactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
