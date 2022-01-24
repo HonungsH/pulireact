ce0557a0598690b9a01c434b31ed37b6d963afbe
Tobias Landelius <tola@netcompany.com>
2022-01-24 14:10:27 +0100

    test push

--- src/main/java/puli/xaidaz/react/pulireact/PulireactApplication.java
+++ src/main/java/puli/xaidaz/react/pulireact/PulireactApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PulireactApplication {
-
+// Test
     public static void main(String[] args) {
         SpringApplication.run(PulireactApplication.class, args);
     }
